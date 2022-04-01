55fefdfd56b6fdb965899326ae83c5805597b0bb
SNVZero <simakov_nikita01@mail.ru>
2022-04-01 21:27:49 +0300

    Second

--- index.php
+++ index.php
@@ -15,105 +15,12 @@
 </head>
 
 <body class="webform">
-
-
-<?php
-
-$regname = '/^.*[^А-яЁё].*$/';
-$err = [];
-$flag = 0;
-
-
-$name = $_POST['name'];
-$email = $_POST['email'];
-$bio = $_POST['bio'];
-
-$date = $_POST['dayofbirth'];
-$test_arr_date  = explode('-', $date);
-
-switch($_POST['gender']) {
-    case 'm': {
-        $gender='m';
-        break;
-    }
-    case 'f':{
-        $gender='f';
-        break;
-    }
-};
-
-switch($_POST['limbs']) {
-    case '2': {
-        $limbs='2';
-        break;
-    }
-    case '4':{
-        $limbs='4';
-        break;
-    }
-    case '8':{
-        $limbs='8';
-        break;
-    }
-    case '16':{
-        $limbs='16';
-        break;
-    }
-};
-
-$power1=in_array('s1',$_POST['capabilities']) ? '1' : '0';
-$power2=in_array('s2',$_POST['capabilities']) ? '1' : '0';
-$power3=in_array('s3',$_POST['capabilities']) ? '1' : '0';
-$power4=in_array('s4',$_POST['capabilities']) ? '1' : '0';
-
-
-if($power1 == 1){
-    $powers1 = 'immortal';
-}else{
-    $powers1 = 'no spell';
-}
-
-if($power2 == 1){
-    $powers2 = 'noclip';
-}else{
-    $powers2 = 'no spell';
-}
-
-if($power3 == 1){
-    $powers3 = 'flying';
-}else{
-    $powers3 = 'no spell';
-}
-
-if($power4 == 1){
-    $powers4 = 'lazer';
-}else{
-    $powers4 = 'no spell';
-}
-
-
-
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    if(!preg_match($regname,$name)){
-        $err['name'] = '<small class=" text-danger">Введите имя кирилицей</small>';
-        $flag = 1;
-    }
-
-    if(empty($name)){
-        $err['name'] ='<small class=" text-danger">Поле не может быть пустым</small>';
-        $flag = 1;
-    }
-}
-?>
-
-
-
     <div class="webform">
         <div class="container">
             <div class="row">
                 <div class="col">
                     <div class="webform__form d-flex justify-content-center">
-                        <form id="form" class="form__body" method="POST">
+                        <form id="form" class="form__body" method="POST" action="send.php">
                             <div>
                                 <input class="webform__form-elem form__input _req" id="names" type="text" name="name"
                                     placeholder="Имя" value="">
