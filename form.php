89026697359ba93aa27198a3490371a65f43ee99
SNVZero <simakov_nikita01@mail.ru>
2022-04-03 21:44:33 +0300

    third

--- form.php
+++ form.php
@@ -1,3 +1,214 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $massage = array();
+
+    if(!empty($_COOKIE['save'])){
+        setcookie('save','',1);
+        $massage['succsess']= TRUE;
+    }
+
+    $error = array();
+
+    $error['name_empty'] = !empty($_COOKIE['name_error_empty']);
+    $error['name'] = !empty($_COOKIE['name_error']);
+    $error['email_empty'] = !empty($_COOKIE['email_error_empty']);
+    $error['email'] = !empty($_COOKIE['email_error']);
+    $error['email'] = !empty($_COOKIE['email_error']);
+    $error['bio'] = !empty($_COOKIE['bio_error']);
+    $error['year'] = !empty($_COOKIE['year_error']);
+    $error['gender'] = !empty($_COOKIE['gender_error']);
+    $error['limbs'] = !empty($_COOKIE['limbs_error']);
+    $error['ability'] = !empty($_COOKIE['ability_error']);
+    $error['agree'] = !empty($_COOKIE['agree_error']);
+
+    if($error['name_empty']){
+        setcookie('name_error_empty','',1);
+        $massage['name_empty'] = TRUE;
+    }
+
+    if($error['name']){
+        setcookie('name_error','',1);
+        $massage['name'] = TRUE;
+    }
+
+    if($error['email_empty']){
+        setcookie('email_error_empty','',1);
+        $massage['email_empty'] = TRUE;
+    }
+
+    if($error['email']){
+        setcookie('email_error','',1);
+        $massage['email'] = TRUE;
+    }
+
+    if($error['bio']){
+        setcookie('bio_error','',1);
+        $massage['bio'] = TRUE;
+    }
+
+    if ($error['year']) {
+        setcookie('year_error', '', 100000);
+        $messages['year'] = TRUE;
+    }
+
+    if ($error['gender']) {
+        setcookie('gender_error', '', 100000);
+        $message['gender'] = TRUE;
+    }
+
+    if ($error['limbs']) {
+        setcookie('limbs_error', '', 100000);
+        $messages['limbs'] = TRUE;
+    }
+
+    if($error['ability']){
+        setcookie('ability_error','',1);
+        $massage['ability'] = TRUE;
+    }
+
+    if($error['agree']){
+        setcookie('agree_error','',1);
+        $massage['agree'] = TRUE;
+    }
+
+    $value = array();
+
+    $value['name'] = empty($_COOKIE['name_value']) ? '' : $_COOKIE['name_value'];
+    $value['email'] = empty($_COOKIE['email_value']) ? '' : $_COOKIE['email_value'];
+    $value['bio'] = empty($_COOKIE['bio_value']) ? '' : $_COOKIE['bio_value'];
+    $value['year'] = empty($_COOKIE['year_value']) ? '' : $_COOKIE['year_value'];
+    $value['gender'] = empty($_COOKIE['gender_value']) ? '' : $_COOKIE['gender_value'];
+    $value['limbs'] = empty($_COOKIE['limbs_value']) ? '' : $_COOKIE['limbs_value'];
+    if(empty($_COOKIE['ability_value']))
+    $values_ability['ability'] = array();
+    else
+    $values_ability = explode(',',$_COOKIE['ability_value']);
+    $value['agree'] = empty($_COOKIE['agree_value']) ? '' : $_COOKIE['agree_value'];
+
+
+}else if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    $regname = '/^.*[^А-яЁё].*$/';
+    $errors = FALSE;
+    $power1=in_array('s1',$_POST['capabilities']) ? '1' : '0';
+    $power2=in_array('s2',$_POST['capabilities']) ? '1' : '0';
+    $power3=in_array('s3',$_POST['capabilities']) ? '1' : '0';
+    $power4=in_array('s4',$_POST['capabilities']) ? '1' : '0';
+
+
+    if($power1 == 1){
+        $ability .= 'immortal' . ',';
+    }
+
+    if($power2 == 1){
+        $ability = 'noclip' . ',';
+    }
+
+    if($power3 == 1){
+        $ability = 'flying' . ',';
+    }
+
+    if($power4 == 1){
+        $ability = 'lazer' . ',';
+    }
+
+    if(empty(htmlentities($_POST['name']))){
+        setcookie('name_error_empty','1',time() + 24 * 60 * 60);
+        $errors = TRUE;
+    }else if(preg_match($regname, $_POST['name'])){
+        setcookie('name_error','1',time() + 24 * 60 * 60);
+        setcookie('name_value',$_POST['name']);
+        $errors = TRUE;
+    }else{
+        setcookie('name_value',$_POST['name'],time() + 24 * 60 * 60 * 30 * 12 );
+    }
+
+    if(empty($_POST['email'])){
+        setcookie('email_error_empty','1',time() + 24 * 60 * 60);
+        $errors = TRUE;
+    }else if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+        setcookie('email_error','1',time() + 24 * 60 * 60);
+        setcookie('email_value',$_POST['email']);
+        $errors = TRUE;
+    }else{
+        setcookie('email_value',$_POST['email'],time() + 24 * 60 * 60 * 30 * 12 );
+    }
+
+    if(empty($_POST['bio'])){
+        setcookie('bio_error','1',time() + 24 * 60 *60);
+        $errors = TRUE;
+    }else{
+        setcookie('bio_value',$_POST['bio'],time() + 24 * 60 * 60 * 30 * 12 );
+    }
+
+
+    if(empty($_POST['year'])){
+        setcookie('year_error','1',time() + 24 * 60 *60);
+        $errors = TRUE;
+    }else{
+        setcookie('year_value',$_POST['year'],time() + 24 * 60 * 60 * 30 * 12 );
+    }
+
+    if(empty($_POST['gender'])){
+        setcookie('gender_error','1',time() + 24 * 60 *60);
+        $errors = TRUE;
+    }else{
+        setcookie('gender_value',$_POST['gender'],time() + 24 * 60 * 60 * 30 * 12 );
+    }
+
+    if(empty($_POST['limbs'])){
+        setcookie('limbs_error','1',time() + 24 * 60 *60);
+        $errors = TRUE;
+    }else{
+        setcookie('limbs_value',$_POST['limbs'],time() + 24 * 60 * 60 * 30 * 12 );
+    }
+
+    if(empty($ability)){
+        setcookie('ability_error','1',time() + 24 * 60 *60);
+        $errors = TRUE;
+    }else{
+        setcookie('ability_value',$ability,time() + 24 * 60 * 60 * 30 * 12 );
+    }
+
+    if (empty($_POST['agree'])) {
+        setcookie('agree_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+      }
+      else {
+        setcookie('agree_value', $_POST['agree'], time() + 12 * 30 * 24 * 60 * 60);
+      }
+
+      if ($errors) {
+        header('Location: index.php');
+        exit();
+      }
+      else {
+        setcookie('name_error_empty','', 1);
+        setcookie('name_error', '', 100000);
+        setcookie('email_error_empty','',1);
+        setcookie('email_error', '', 1);
+        setcookie('bio_error','', 1);
+        setcookie('year_error', '', 1);
+        setcookie('gender_error', '', 1);
+        setcookie('limbs_error', '', 1);
+        setcookie('ability_error','', 1);
+        setcookie('checkbox_error', '', 1);
+
+        setcookie('save','1');
+
+        header('Location: index.php');
+      }
+
+
+
+
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -11,7 +222,6 @@
 </head>
 
 <body class="webform">
-
     <div class="webform">
         <div class="container">
             <div class="row">
