607ca75fc14b491459fb6f7dfc4fa130c23e70dd
SNVZero <simakov_nikita01@mail.ru>
2022-04-03 21:34:30 +0300

    third

--- index.php
+++ index.php
@@ -1,94 +1,210 @@
-<!DOCTYPE html>
-<html>
+<?php
+header('Content-Type: text/html; charset=UTF-8');
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Задание 4</title>
-    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
-</head>
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $massage = array();
 
-<body class="webform">
+    if(!empty($_COOKIE['save'])){
+        setcookie('save','',1);
+        $massage['succsess']= TRUE;
+    }
 
-    <?php
-    include('send.php');
-    ?>
+    $error = array();
 
-    <div class="webform">
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <div class="webform__form d-flex justify-content-center">
-                        <form id="form" class="form__body" method="POST">
-                            <div>
-                                <input class="webform__form-elem form__input _req" id="names" type="text" name="name"
-                                    placeholder="Имя" value="">
-                                <?php echo $err['name'] ?>
-                            </div>
-                            <div>
-                                <input class="webform__form-elem form__input _req _email" id="email" type="email"
-                                    name="email" placeholder="E-mail">
-                            </div>
-                            <div>
-                                <textarea id="comment" class="webform__form-elem form__input _req" type="text"
-                                    name="bio" placeholder="Биография"></textarea>
-                            </div>
-                            <div class="form_item form-group">
-                                <label for="formDate" style="color: white;">Дата рождения:</label>
-                                <input type="date"
-                                    class="form_input form__input _req form-control w-50  bg-white rounded"
-                                    name="dayofbirth" id="dates" value="">
-                            </div>
-                            <div class="gender">
-                                <label style="margin-right: 5px;">Пол : </label>
-                                <div>
-                                    <input type="radio" id="male" name="gender" value="m" checked />
-                                    <label for="male" id="male">мужской</label>
-                                </div>
-                                <div>
-                                    <input type="radio" id="female" name="gender" value="f" />
-                                    <label for="female" id="female">женский</label>
-                                </div>
-                            </div>
-                            <div class="limbs">
-                                <label>Количество конечностей :</label>
-                                <input type="radio" id="2" name="limbs" value="2">
-                                <label for="2" id="2">2</label>
-                                <input type="radio" id="4" name="limbs" value="4" checked>
-                                <label for="4" id="4">4</label>
-                                <input type="radio" id="8" name="limbs" value="8">
-                                <label for="8" id="8">8</label>
-                                <input type="radio" id="16" name="limbs" value="16">
-                                <label for="16" id="16">16</label>
-                            </div>
-                            <div class="capabilities">
-                                <select name="capabilities[]" size="2" multiple required>
-                                    <option value="s1">бессмертие</option>
-                                    <option value="s2">прохождение сквозь стены</option>
-                                    <option value="s3">левитация</option>
-                                    <option value="s4">лазеры из глаз</option>
-                                </select>
-                            </div>
-                            <div class="form__checkbox">
-                                <input class="checkbox__input _req" type="checkbox" id="userAgreement" name="agree">
-                                <label class="checkbox__label" for="userAgreement">Отправляя заявку, я даю согласие
-                                    на<a>обработку своих персональных данных</a>.<span>*</span></label>
-                            </div>
-                            <div>
-                                <input class="webform__form-btn" type="submit" name="submit" value="Отправить">
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
+    $error['name_empty'] = !empty($_COOKIE['name_error_empty']);
+    $error['name'] = !empty($_COOKIE['name_error']);
+    $error['email_empty'] = !empty($_COOKIE['email_error_empty']);
+    $error['email'] = !empty($_COOKIE['email_error']);
+    $error['email'] = !empty($_COOKIE['email_error']);
+    $error['bio'] = !empty($_COOKIE['bio_error']);
+    $error['year'] = !empty($_COOKIE['year_error']);
+    $error['gender'] = !empty($_COOKIE['gender_error']);
+    $error['limbs'] = !empty($_COOKIE['limbs_error']);
+    $error['ability'] = !empty($_COOKIE['ability_error']);
+    $error['agree'] = !empty($_COOKIE['agree_error']);
 
-</html>
+    if($error['name_empty']){
+        setcookie('name_error_empty','',1);
+        $massage['name_empty'] = TRUE;
+    }
+
+    if($error['name']){
+        setcookie('name_error','',1);
+        $massage['name'] = TRUE;
+    }
+
+    if($error['email_empty']){
+        setcookie('email_error_empty','',1);
+        $massage['email_empty'] = TRUE;
+    }
+
+    if($error['email']){
+        setcookie('email_error','',1);
+        $massage['email'] = TRUE;
+    }
+
+    if($error['bio']){
+        setcookie('bio_error','',1);
+        $massage['bio'] = TRUE;
+    }
+
+    if ($error['year']) {
+        setcookie('year_error', '', 100000);
+        $messages['year'] = TRUE;
+    }
+
+    if ($error['gender']) {
+        setcookie('gender_error', '', 100000);
+        $message['gender'] = TRUE;
+    }
+
+    if ($error['limbs']) {
+        setcookie('limbs_error', '', 100000);
+        $messages['limbs'] = TRUE;
+    }
+
+    if($error['ability']){
+        setcookie('ability_error','',1);
+        $massage['ability'] = TRUE;
+    }
+
+    if($error['agree']){
+        setcookie('agree_error','',1);
+        $massage['agree'] = TRUE;
+    }
+
+    $value = array();
+
+    $value['name'] = empty($_COOKIE['name_value']) ? '' : $_COOKIE['name_value'];
+    $value['email'] = empty($_COOKIE['email_value']) ? '' : $_COOKIE['email_value'];
+    $value['bio'] = empty($_COOKIE['bio_value']) ? '' : $_COOKIE['bio_value'];
+    $value['year'] = empty($_COOKIE['year_value']) ? '' : $_COOKIE['year_value'];
+    $value['gender'] = empty($_COOKIE['gender_value']) ? '' : $_COOKIE['gender_value'];
+    $value['limbs'] = empty($_COOKIE['limbs_value']) ? '' : $_COOKIE['limbs_value'];
+    if(empty($_COOKIE['ability_value']))
+    $values_ability['ability'] = array();
+    else
+    $values_ability = explode(',',$_COOKIE['ability_value']);
+    $value['agree'] = empty($_COOKIE['agree_value']) ? '' : $_COOKIE['agree_value'];
+
+    include('form.php');
+}else if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    $regname = '/^.*[^А-яЁё].*$/';
+    $errors = FALSE;
+    $power1=in_array('s1',$_POST['capabilities']) ? '1' : '0';
+    $power2=in_array('s2',$_POST['capabilities']) ? '1' : '0';
+    $power3=in_array('s3',$_POST['capabilities']) ? '1' : '0';
+    $power4=in_array('s4',$_POST['capabilities']) ? '1' : '0';
+
+
+    if($power1 == 1){
+        $ability .= 'immortal' . ',';
+    }
+
+    if($power2 == 1){
+        $ability = 'noclip' . ',';
+    }
+
+    if($power3 == 1){
+        $ability = 'flying' . ',';
+    }
+
+    if($power4 == 1){
+        $ability = 'lazer' . ',';
+    }
+
+    if(empty(htmlentities($_POST['name']))){
+        setcookie('name_error_empty','1',time() + 24 * 60 * 60);
+        $errors = TRUE;
+    }else if(preg_match($regname, $_POST['name'])){
+        setcookie('name_error','1',time() + 24 * 60 * 60);
+        setcookie('name_value',$_POST['name']);
+        $errors = TRUE;
+    }else{
+        setcookie('name_value',$_POST['name'],time() + 24 * 60 * 60 * 30 * 12 )
+    }
+
+    if(empty($_POST['email'])){
+        setcookie('email_error_empty','1',time() + 24 * 60 * 60);
+        $errors = TRUE;
+    }else if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+        setcookie('email_error','1',time() + 24 * 60 * 60);
+        setcookie('email_value',$_POST['email']);
+        $errors = TRUE;
+    }else{
+        setcookie('email_value',$_POST['email'],time() + 24 * 60 * 60 * 30 * 12 );
+    }
+
+    if(empty($_POST['bio'])){
+        setcookie('bio_error','1',time() + 24 * 60 *60);
+        $errors = TRUE;
+    }else{
+        setcookie('bio_value',$_POST['bio'],time() + 24 * 60 * 60 * 30 * 12 );
+    }
+
+
+    if(empty($_POST['year'])){
+        setcookie('year_error','1',time() + 24 * 60 *60);
+        $errors = TRUE;
+    }else{
+        setcookie('year_value',$_POST['year'],time() + 24 * 60 * 60 * 30 * 12 );
+    }
+
+    if(empty($_POST['gender'])){
+        setcookie('gender_error','1',time() + 24 * 60 *60);
+        $errors = TRUE;
+    }else{
+        setcookie('gender_value',$_POST['gender'],time() + 24 * 60 * 60 * 30 * 12 );
+    }
+
+    if(empty($_POST['limbs'])){
+        setcookie('limbs_error','1',time() + 24 * 60 *60);
+        $errors = TRUE;
+    }else{
+        setcookie('limbs_value',$_POST['limbs'],time() + 24 * 60 * 60 * 30 * 12 );
+    }
+
+    if(empty($ability)){
+        setcookie('ability_error','1',time() + 24 * 60 *60);
+        $errors = TRUE;
+    }else{
+        setcookie('ability_value',$ability,time() + 24 * 60 * 60 * 30 * 12 );
+    }
+
+    if (empty($_POST['agree'])) {
+        setcookie('agree_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+      }
+      else {
+        setcookie('agree_value', $_POST['agree'], time() + 12 * 30 * 24 * 60 * 60);
+      }
+
+      if ($errors) {
+        header('Location: index.php');
+        exit();
+      }
+      else {
+        setcookie('name_error_empty','', 1);
+        setcookie('name_error', '', 100000);
+        setcookie('email_error_empty','',1);
+        setcookie('email_error', '', 1);
+        setcookie('bio_error','', 1);
+        setcookie('year_error', '', 1);
+        setcookie('gender_error', '', 1);
+        setcookie('limbs_error', '', 1);
+        setcookie('ability_error','', 1);
+        setcookie('checkbox_error', '', 1);
+
+        setcookie('save','1');
+
+        header('Location: index.php');
+      }
+
+
+
+
+
+}
+
+?>
