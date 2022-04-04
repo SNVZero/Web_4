01e66c4adae49c6f4a9fc141ea9682e86e336788
SNVZero <simakov_nikita01@mail.ru>
2022-04-04 17:20:48 +0300

    fourth

--- index.php
+++ index.php
@@ -232,12 +232,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             $stmt = $db->prepare("INSERT INTO people SET name = ?, mail = ?, bio = ?, date = ?, gender = ?, limbs = ?");
             $stmt -> execute([$_POST['name'], $_POST['email'], $_POST['bio'], $_POST['year'], $_POST['gender'], $_POST['limbs']]);
 
-            $res = $db->query("SELECT COALESCE(MAX(id), 0) FROM people");
-            $row = $res->fetch();
-            $count = (int)$row[0];
-
-            $stmt = $db->prepare("INSERT INTO ability SET human_id = ?, superabilities  = ?");
-            $stmt -> execute([$count, $value_ability]);
 
             $message['succsess']= TRUE;
 
